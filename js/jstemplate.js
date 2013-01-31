5d5657155313508b00b62965de569ab79353e4d2
criscom <christoph.potzinger@gmail.com>
2013-01-31 13:14:45 +0100

    JQuery Datei mit einbezogen

---
+++ js/jstemplate.js
@@ -0,0 +1,11 @@
+/* JQuery Template für wohnservice.at - alle JQuery Befehle */
+
+(function ($) {
+  $(document).ready(function() {
+	
+//	$("div.projekt-bild-gross").children("a").addClass("colorbox init-colorbox-processed cboxElement");
+//	$("div.projekt-bild-gross").children("a").attr('rel', 'galerie_projekte');
+//	$("div.projekt-bild-gross").children("a").children('span').children('img').add("abc");
+	});
+
+})(jQuery);
