a4c40ea5aef3dd2ea65c517aef784297287a1085
criscom <christoph.potzinger@gmail.com>
2013-02-21 12:35:19 +0100

    Neues Template für Bauträger Detail
    Nachdem ich den Ordner wohnservices lokal verschoben hatte, gab's mit
    git Probleme. Ursprünglichen Zustand wieder hergestellt.

---
+++ ds_layouts/bautraeger_2col_stacked/bautraeger_2col_stacked.tpl.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 2 column stacked template.
+ */
+?>
+<div class="bautraeger-detail-2col-stacked <?php print $classes;?> clearfix">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php if ($header): ?>
+    <div class="group-header<?php print $header_classes; ?>">
+      <?php print $header; ?>
+    </div>
+  <?php endif; ?>
+<!-- ><div id="container3">
+<div id="container2"> -->
+<div id="container1">
+  <?php if ($left): ?>
+    <div class="group-left<?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($right): ?>
+    <div class="group-right<?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </div>
+  <?php endif; ?>
+</div> <!-- end container1 -->
+<!-- </div>  end container2 -->
+ <!-- </div> end container3 -->
+
+  <?php if ($footer): ?>
+    <div class="group-footer<?php print $footer_classes; ?>">
+      <?php print $footer; ?>
+    </div>
+  <?php endif; ?>
+</div>
