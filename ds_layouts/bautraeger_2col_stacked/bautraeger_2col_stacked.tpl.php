6b059edb23d2bd2c5c5c5346e23642e0d34f74cb
root <root@vserver.(none)>
2013-02-22 11:29:52 +0100

    DominikFrontpage 1. Tag/Nacht

---
+++ ds_layouts/bautraeger_2col_stacked/bautraeger_2col_stacked.tpl.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 2 column stacked template.
+ */
+?>
+<div class="bautraeger-detail-2col-stacked <?php print $classes;?> clearfix">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php if ($header): ?>
+    <div class="group-header<?php print $header_classes; ?>">
+      <?php print $header; ?>
+    </div>
+  <?php endif; ?>
+<!-- ><div id="container3">
+<div id="container2"> -->
+<div class="container1">
+  <?php if ($left): ?>
+    <div class="group-left<?php print $left_classes; ?>">
+      <?php print $left; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($right): ?>
+    <div class="group-right<?php print $right_classes; ?>">
+      <?php print $right; ?>
+    </div>
+    <div class="clear-float">&nbsp;</div>
+  <?php endif; ?>
+</div> <!-- end container1 -->
+<!-- </div> --> <!-- end container2 -->
+ <!-- </div> --> <!-- end container3 -->
+
+  <?php if ($footer): ?>
+    <div class="group-footer<?php print $footer_classes; ?>">
+      <div class="footer-wrapper">
+	      <?php print $footer; ?>    
+     </div> <!-- close footer wrapper -->
+     <div class="clear-float">&nbsp;</div>
+    </div>
+  <?php endif; ?>
+</div>
--- js/jstemplate.js
+++ js/jstemplate.js
@@ -6,6 +6,13 @@
 	$("div.projekt-bild-gross").children("a").addClass("colorbox init-colorbox-processed cboxElement");
 	$("div.projekt-bild-gross").children("a").attr('rel', 'galerie_projekte');
 //	$("div.projekt-bild-gross").children("a").children('span').children('img').add("abc");
+		
+/*		var a = $('body.page-frontpage .flexslider-views-slideshow-main-frame-row > div');
+		for( var i = 0; i < a.length; i+=3 ) {
+		    a.slice(i, i+2).wrapAll('<div class="front-slideshow-beschreibung"></div>');
+		}*/
+		
+		
 	});
 
 })(jQuery);
