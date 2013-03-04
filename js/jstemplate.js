27c7c61ab0c8c2feb786facd98653e5967f0f921
Christoph Potzinger <criscom27@gmail.com>
2013-03-04 10:12:28 +0100

    Filter und Projekte Detail 1. Teil von Dominik

--- js/jstemplate.js
+++ js/jstemplate.js
@@ -3,8 +3,8 @@
 (function ($) {
   $(document).ready(function() {
 
-	$("div.projekt-bild-gross").children("a").addClass("colorbox init-colorbox-processed cboxElement");
-	$("div.projekt-bild-gross").children("a").attr('rel', 'galerie_projekte');
+		$("div.projekt-bild-gross").children("a").addClass("colorbox init-colorbox-processed cboxElement");
+		$("div.projekt-bild-gross").children("a").attr('rel', 'galerie_projekte');
 //	$("div.projekt-bild-gross").children("a").children('span').children('img').add("abc");
 		
 /*		var a = $('body.page-frontpage .flexslider-views-slideshow-main-frame-row > div');
@@ -12,6 +12,23 @@
 		    a.slice(i, i+2).wrapAll('<div class="front-slideshow-beschreibung"></div>');
 		}*/
 		
+		//Projektseite
+		if ($('#edit-field-groesse-value-min').length > 0) {
+			if (!$('#edit-field-groesse-value-min').val().match("m2$")) {
+				$('#edit-field-groesse-value-min').val($('#edit-field-groesse-value-min').val()+" m2");
+			}
+			
+			if (!$('#edit-field-groesse-value-max').val().match("m2$")) {
+				$('#edit-field-groesse-value-max').val($('#edit-field-groesse-value-max').val()+" m2");
+			}
+			
+			$('.view-projekt-bersicht-frei-finanzierte-projekte .pager-previous a').html("zur&uuml;ck");
+			$('.view-projekt-bersicht-frei-finanzierte-projekte .pager-next a').html("n&auml;chste");
+		}
+		
+		/*if ($('#block-system-main .group-header .field-name-anfrage').length > 0) {
+			$('#block-system-main .group-header').before($('#block-system-main .group-header .field-name-anfrage'));
+		}*/
 		
 	});
 
