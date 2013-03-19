e84c34ba775f7226ca61dd57ddb693f8ee9fb3f2
Christoph Potzinger <criscom27@gmail.com>
2013-03-19 20:51:37 +0100

    Wohntypen Detail von Dominik fertig gestellt

--- js/jstemplate.js
+++ js/jstemplate.js
@@ -27,6 +27,8 @@
 			$('.view-projekt-bersicht-frei-finanzierte-projekte .pager-next a').html("n&auml;chste");
 		}
 		
+		$('.pager-previous a').html("zur&uuml;ck");
+		$('.pager-next a').html("n&auml;chste");
 		
 		/*if ($('#block-system-main .group-header .field-name-anfrage').length > 0) {
 			$('#block-system-main .group-header').before($('#block-system-main .group-header .field-name-anfrage'));
@@ -60,6 +62,14 @@
 			});
 		});
 		
+		$('.field-name-field-wohnungstyp-bilder-klein a').mouseenter(function() {
+			$('div.imghover').remove();
+			$(this).append('<div class="imghover"></div>');
+			$(this).find('div.imghover').mouseleave(function() {
+				$('div.imghover').remove();
+			});
+		});
+		
 		//Projektdetailseite unten: Höhe bestimmen und entsprechend die anderen zwei Spalten anpassen
 		var heightleft = $('.node-projekte .group-left').height();
 		var heightmiddle = $('.node-projekte .group-middle').height();
@@ -85,6 +95,22 @@
 		$('.aktuelle-projekte-field-item .field-name-field-projekt-bild a').append('<div class="projektimghover"></div>');
 		$('.weitere-projekte-field .field-name-field-projekt-bild a').append('<div class="projektimghover2"></div>');
 		
+		//Typenseite unten: Höhe bestimmen und anpassen
+		$('.node-type-wohnungstype .group_wohnungstypen_kaufdaten').append('<div class="clear"></div>');
+		var heightleft = $('.node-type-wohnungstype .group-left').height();
+		var heightmiddle = $('.node-type-wohnungstype .group-middle').height();
+		var heightright = $('.node-type-wohnungstype .group-right').height();
+		if ((heightleft >= heightmiddle) && (heightleft >= heightright))
+		{
+			$('.node-type-wohnungstype .group-middle').height(heightleft);
+			$('.node-type-wohnungstype .group-right').height(heightleft);
+		} else if ((heightmiddle >= heightleft) && (heightmiddle >= heightright)) {
+			$('.node-type-wohnungstype .group-left').height(heightmiddle);
+			$('.node-type-wohnungstype .group-right').height(heightmiddle);
+		} else if ((heightright >= heightmiddle) && (heightright >= heightleft)) {
+			$('.node-type-wohnungstype .group-middle').height(heightright-16);
+			$('.node-type-wohnungstype .group-left').height(heightright-16);
+		}
 	});
 
 })(jQuery);
